b9b261699d0dc5c4845253d9c75ac6698e3fb37c
falarcon13 <feralar13@gmail.com>
2023-11-18 11:31:20 -0600

    Summary

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Title } from '@angular/platform-browser';
   template: '<router-outlet></router-outlet>',
 })
 export class AppComponent implements OnInit {
-  title = 'CoreUI Free Angular Admin Template';
+  title = 'Gestion de Asistencia - Ceutec';
 
   constructor(
     private router: Router,
